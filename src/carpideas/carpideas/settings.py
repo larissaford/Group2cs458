f096cb621268f8461429ebf9351900bdeb413237
joe <sedivyj0322@my.uwstout.edu>
2020-09-28 20:46:47 -0500

    Routing and Settings Updated settings to allow for templates to exist in format appName/templates/viewHTMLocation, also added routing to register page

--- src/carpideas/carpideas/settings.py
+++ src/carpideas/carpideas/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Our Custom Apps
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'carpideas.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # Tells Django where to look for templates
+        # Structures it to have appName/templates/viewHTMLLocation
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
