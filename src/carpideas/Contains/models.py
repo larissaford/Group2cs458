aec11b77cf5fdd24c2453ff81a2b2474f6bdbaa4
Ford <fords7798@my.uwstout.edu>
2020-10-07 12:00:49 -0500

    added database

--- src/carpideas/Contains/models.py
+++ src/carpideas/Contains/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from Image.models import Image
-from accounts.models import Accounts
+from .models import Image
 
 
 # Create your models here.
 class Contains(models.Model):
-    account = models.ForeignKey(Accounts, on_delete=models.CASCADE,)
-    image = models.ForeignKey(Image, on_delete=models.CASCADE,)
+    accountImages = models.ManyToManyField(Image)
