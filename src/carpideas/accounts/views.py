0f21e209a8120469ccef8aa8ab9cf2873d12ccaf
meierm7595 <meierm7595@my.uwstout.edu>
2020-10-07 13:05:28 -0500

    Login Page
    Created login.html, edited urls.py and views.py

--- src/carpideas/accounts/views.py
+++ src/carpideas/accounts/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse # nice for testing
 from django.shortcuts import render, redirect
 
 from .forms import RegisterForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from django.contrib.auth import authenticate, login, logout
 
 from django.contrib import messages
 
@@ -26,4 +28,30 @@ def register_view(request):
         "form": register_form
     }
     return render(request, 'accounts/register.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect('login')
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('register')
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+    form = AuthenticationForm()
+    return render(request, 
+                    'accounts/login.html',
+                    {'form':form})
 
