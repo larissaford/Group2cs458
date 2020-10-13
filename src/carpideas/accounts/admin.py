72110efca4e2c545b07b0f2bfa09ea05021127c8
joe <sedivyj0322@my.uwstout.edu>
2020-10-12 21:22:14 -0500

    Removed Account Object Account not necessary. Django's User is satisfactory

--- src/carpideas/accounts/admin.py
+++ src/carpideas/accounts/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Account
-
-admin.site.register(Account)
--- src/carpideas/accounts/forms.py
+++ src/carpideas/accounts/forms.py
@@ -3,9 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
 
-from .models import Account
-
-
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
@@ -15,6 +12,3 @@ class RegisterForm(UserCreationForm):
             'password1',
             'password2'
         ]
-    # username = forms.CharField(max_length=16)
-    # password = forms.PasswordInput()
-    # email = forms.EmailField()
