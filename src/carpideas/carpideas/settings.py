3ac5edf070ff63307a50db16c4544717927e3d26
Ford <fordl8861@my.uwstout.edu>
2020-10-16 11:20:27 -0500

    added a home page and redirected our server's home page to the login page

--- src/carpideas/carpideas/settings.py
+++ src/carpideas/carpideas/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "accounts",
     'Image',
     'Contains',
+    'home'
 ]
 
 MIDDLEWARE = [
--- src/carpideas/carpideas/urls.py
+++ src/carpideas/carpideas/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 
 
 from accounts.views import activate_view, register_view, login_view, logout_view
+from home.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +29,6 @@ urlpatterns = [
     path('register/', register_view, name="register"),
     path('login/', login_view, name="login"),
     path('logout/', logout_view, name="logout"),
+    path('home/', home_view, name='home'),
+    path('', login_view, name="login")
 ]
