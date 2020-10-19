43145131f9f46a7020a2a92988e4ff0986c55f55
Ford <fordl8861@my.uwstout.edu>
2020-10-19 13:02:59 -0500

    initial commit

--- src/carpideas/home/views.py
+++ src/carpideas/home/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello World</h1>")
-    return render(request, "base.html", {})
+
+    #contains a key-value pair
+    my_context = {
+        "my_text": "this is about me"
+    }
+
+    return render(request, "home.html", my_context)
