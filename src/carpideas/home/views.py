700c6b5ee2e6f87dc4d6b2525153910d7976ec45
Ford <fordl8861@my.uwstout.edu>
2020-10-20 11:55:16 -0500

    got rid of arguments from home_view

--- src/carpideas/home/views.py
+++ src/carpideas/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(request, *args, **kwargs):
+def home_view(request):
     #return HttpResponse("<h1>Hello World</h1>")
 
     #contains a key-value pair
