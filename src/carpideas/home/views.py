de5826032419a8072aef8d7792dc03b860863197
joe <sedivyj0322@my.uwstout.edu>
2020-10-28 22:22:33 -0500

    Redirect from home Will redirect to login page if a user is not logged in

--- src/carpideas/home/views.py
+++ src/carpideas/home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from PIL import Image
@@ -18,8 +18,11 @@ def home_view(request):
     my_context = {
     #    'username' : user.username
     }
-    
-    return render(request, "home.html", my_context)
+    # Check if user is anonymous user
+    if not request.user.is_authenticated:
+        return redirect("login")
+    else:
+        return render(request, "home.html", my_context)
 
 def pixelate_image(request):
         
