c572f03918dbe7804d0e7a929f5945c8d73dedc1
Ford <fords7798@my.uwstout.edu>
2020-10-30 16:53:29 -0500

    preliminary spotify and clock widget

--- src/carpideas/home/static/javascript/base.js
+++ src/carpideas/home/static/javascript/base.js
@@ -20,4 +20,19 @@ function openForm() {
     document.getElementById("myForm").style.display = "none";
   }
 
-  
+
+
+function showclock() {
+  let today = new Date();
+  let hours = today.getHours();
+  let mins = today.getMinutes();
+  let seconds = today.getSeconds();
+  const addZero = num => {
+    if(num < 10) { return '0' + num };
+    return num;
+  }
+  $('#hour').text(addZero(hours));
+  $('#min').text(addZero(mins));
+  $('#second').text(addZero(seconds));
+}
+setInterval(showclock, 1000);
