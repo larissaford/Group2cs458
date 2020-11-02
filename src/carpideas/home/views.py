2fa7a96a4fccd36226e986e8ba39b3a500fe30df
meierm7595 <meierm7595@my.uwstout.edu>
2020-11-02 13:04:42 -0600

    QuoteOfTheDay
    all of quote of the day task besides the skip quote button. Might change some design aspects later on and add more quotes to database.

--- src/carpideas/home/views.py
+++ src/carpideas/home/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from .models import Quotes
+import random
 
 # Create your views here.
+
+
+
 def home_view(request):
-    #return HttpResponse("<h1>Hello World</h1>")
-
-    #user = User.objects.get(id=1)
-
-    #contains a key-value pair
-    my_context = {
-    #    'username' : user.username
-    }
-    
-    return render(request, "home.html", my_context)
+	#return HttpResponse("<h1>Hello World</h1>")
+	randNum = random.randint(0,16) 
+	#user = User.objects.get(id=1)
+	posts = Quotes.objects.get(quotesID=randNum)
+	 
+	my_context = {
+	#    'username' : user.username
+		'quote': posts.quote
+	}
+	
+	return render(request, "home.html", my_context)
