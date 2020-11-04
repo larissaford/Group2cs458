839f5be17de756da8d827adccd8db5f8cd34105d
vailtra <60304598+vailtra@users.noreply.github.com>
2020-11-04 14:44:26 -0600

    second
    sdfs

--- src/carpideas/Image/models.py
+++ src/carpideas/Image/models.py
@@ -12,10 +12,24 @@ from django.contrib.auth.models import User
     #def __unicode__(self):
     #    return self.name
 
-class UserInput(models.Model):
-    search = models.CharField(max_length)
-    
-
-class SearchURL(models.Model):
-    url=models.URLField()
-    seen = models.
+# Create your models here.
+class SearchQuery(models.Model):
+	#what the user is searching
+	userSearchQuery = models.charField(max_length=255,unique =true)
+	#when was the last time the user seached this topic
+	lastSearched  = models.DateTimeField(auto_now_add=true)
+	# this makes this seach unique to the user that is seaching this query
+	userWhoSearched = models.ForeignKey(User, related_name='SearchQuery',on_delete=models.CASCADE)
+	
+class ImageURL(models.Model):
+	#url is the url path for the image in its database
+	url = models.charField(max_length=255, unique = true)
+	#when was the last time that the user saw this image
+	#when ImageSeenOn is Null then that will mean that the user has never seen the image befor
+	ImageSeenOn= models.DateTimeField(auto_now_add= true)
+	#the user will be able to see these again with no time constrain
+	ImageLiked = models.boole()
+	#this will keep track of all the images that the user does not wish to seen again
+	ImageDisliked = models.boole()
+	#this is the foreignkey that links Image url to that of SearchQuery
+	SearchQuery = models.ForeignKey(SearchQuery, related_name='ImageURL', on_delete=models.CASCADE)
