f673a7794979604cd9ccee9dddfe490e3b9e891d
joe <sedivyj0322@my.uwstout.edu>
2020-11-04 15:51:01 -0600

    Uncommented Function Should've been uncommented in my previous commit

--- src/carpideas/home/models.py
+++ src/carpideas/home/models.py
@@ -6,5 +6,5 @@ class Quote(models.Model):
 	quoteID = models.CharField(max_length=16, primary_key= True)
 	quote = models.CharField(max_length=300)
 
-	#def __str__(self):
-	#	return self.quote
+	def __str__(self):
+		return self.quote
