309b824686b5b5e311fbf83aa5337532eb47e2b4
joe <sedivyj0322@my.uwstout.edu>
2020-11-13 01:28:51 -0600

    Comments for Account app

--- src/carpideas/accounts/forms.py
+++ src/carpideas/accounts/forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from accounts.models import CustomUser
 
 
+# Form to register an account on the site
 class RegisterForm(UserCreationForm):
     class Meta:
         model = CustomUser
@@ -15,7 +16,7 @@ class RegisterForm(UserCreationForm):
             'password2'
         ]
 
-
+# Form for updating user information in the admin portal
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
